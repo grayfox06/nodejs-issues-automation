c8859466d9437be8a92bc9d6f64686777696d14c
Milos Ilic <milosilic89@outlook.com>
2023-09-14 12:27:29 +0200

    Added Delete All Issues Functionality

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ db.serialize(() => {
   );
 });
 
+// Create a simple 'SupportAgents' table
+db.serialize(() => {
+  db.run(
+    'CREATE TABLE IF NOT EXISTS support_agents (id INTEGER PRIMARY KEY, isAvailable INTEGER)'
+  );
+});
+
+// Initialize support agents
+db.run('INSERT OR IGNORE INTO support_agents (isAvailable) VALUES (1)');
+
 // API to report an issue
 app.post('/api/issues', (req, res) => {
   const { description } = req.body;
@@ -22,15 +32,39 @@ app.post('/api/issues', (req, res) => {
     return res.status(400).json({ error: 'Description is required' });
   }
 
-  // Simulate automatic assignment to a free agent (for simplicity)
-  db.run(
-    'INSERT INTO issues (description, status, agentId) VALUES (?, ?, ?)',
-    [description, 'open', 1],
-    function (err) {
-      if (err) {
-        return res.status(500).json({ error: 'Failed to create an issue' });
+  // Find an available support agent
+  db.get(
+    'SELECT id FROM support_agents WHERE isAvailable = 1',
+    (agentErr, agentRow) => {
+      if (agentErr) {
+        return res
+          .status(500)
+          .json({ error: 'Failed to find an available support agent' });
       }
-      res.status(201).json({ message: 'Issue reported successfully' });
+
+      if (!agentRow) {
+        return res.status(500).json({ error: 'No available support agents' });
+      }
+
+      const agentId = agentRow.id;
+
+      // Simulate automatic assignment to a free agent (for simplicity)
+      db.run(
+        'INSERT INTO issues (description, status, agentId) VALUES (?, ?, ?)',
+        [description, 'open', agentId],
+        function (err) {
+          if (err) {
+            return res.status(500).json({ error: 'Failed to create an issue' });
+          }
+
+          // Mark the support agent as busy
+          db.run('UPDATE support_agents SET isAvailable = 0 WHERE id = ?', [
+            agentId,
+          ]);
+
+          res.status(201).json({ message: 'Issue reported successfully' });
+        }
+      );
     }
   );
 });
@@ -47,6 +81,27 @@ app.put('/api/issues/:id', (req, res) => {
       if (err) {
         return res.status(500).json({ error: 'Failed to resolve the issue' });
       }
+
+      // Mark the support agent as available
+      db.get(
+        'SELECT agentId FROM issues WHERE id = ?',
+        [id],
+        (agentErr, agentRow) => {
+          if (agentErr) {
+            return res
+              .status(500)
+              .json({ error: 'Failed to find the support agent' });
+          }
+
+          if (agentRow) {
+            const agentId = agentRow.agentId;
+            db.run('UPDATE support_agents SET isAvailable = 1 WHERE id = ?', [
+              agentId,
+            ]);
+          }
+        }
+      );
+
       res.json({ message: 'Issue marked as resolved' });
     }
   );
@@ -59,6 +114,27 @@ app.get('/api/issues', (req, res) => {
       return res.status(500).json({ error: 'Failed to fetch issues' });
     }
     res.json(rows);
+  });
+});
+
+// API to delete all issues
+app.delete('/api/issues', (req, res) => {
+  // Delete all issues from the 'issues' table
+  db.run('DELETE FROM issues', function (err) {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to delete issues' });
+    }
+
+    // Reset support agents' availability to 'available' after deleting issues
+    db.run('UPDATE support_agents SET isAvailable = 1', function (agentErr) {
+      if (agentErr) {
+        return res
+          .status(500)
+          .json({ error: 'Failed to reset agent availability' });
+      }
+
+      res.json({ message: 'All issues deleted successfully' });
+    });
   });
 });
 
